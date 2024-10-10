6e8aa3295fa3943671ef50ef830f664e6a275582
Davide Cavallari <137596685+davidecavallari@users.noreply.github.com>
2024-10-10 11:58:24 +0000

    Primo esperimento python (testi e interi)

---
+++ moltiplicazione.py
@@ -0,0 +1,13 @@
+"""
+Programma che legge due 
+numeri in input e li moltiplica
+solo se sono entrambi positivi
+"""
+
+x = input("Inserisci il primo numero (positivo): ")
+y = input("Inserisci il secondo numero (positivo): ")
+
+x = int(x)
+y = int(y)
+
+print(x + y)
